da88c3098191605f2c499009874df1ee1de01dea
nasikmohammed <nasikmohammed17@gmail.com>
2024-03-11 17:09:41 +0530

    first commit

---
+++ bin/trello_1.dart
@@ -0,0 +1,33 @@
+void main() {
+  addonee();
+   multipy();
+}
+
+multipy() {
+  var num1 = "123";
+  var num2 = "456";
+  var num11 = int.parse(num1);
+  var num22 = int.parse(num2);
+  var result = num11 * num22;
+  print(result);
+}
+
+
+
+addonee() {
+  List<int> digits = [1, 2, 3];
+
+  int carry = 1;
+  for (int i = digits.length - 1; i >= 0; i--) {
+    int sum = digits[i] + carry;
+    digits[i] = sum % 10;
+    carry = sum ~/ 10;
+    if (carry == 0) {
+      break;
+    }
+  }
+  if (carry > 0) {
+    digits.insert(0, carry);
+  }
+  print(digits);
+}
