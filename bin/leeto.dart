ba84f9558e0997f7d5ff72fece53f508bf982ff0
nasikmohammed <nasikmohammed17@gmail.com>
2024-03-21 16:44:59 +0530

    your message for this commit

--- bin/leeto.dart
+++ bin/leeto.dart
@@ -1,13 +1,1 @@
-import 'package:test/test.dart';
 
-leeto() {
-  int count = 0;
-  String Haystack = "leetcode";
-  String Needle = "leetoo";
-
-  if (Haystack.contains(Needle)) {
-    print(count);
-  } else {
-    print(count-1);
-  }
-}
--- bin/trello_1.dart
+++ bin/trello_1.dart
@@ -51,3 +51,31 @@ sumofarray() {
 
   print(B);
 }
+// Find the index of the first occurrence in a string;-
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+  leeto() {
+  int count = 0;
+  String Haystack = "leetcode";
+  String Needle = "leetoo";
+
+  if (Haystack.contains(Needle)) {
+    print(count);
+  } else {
+    print(count-1);
+  }
+}
